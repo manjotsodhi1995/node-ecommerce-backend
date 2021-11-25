99fb52fa6cc0c4e0ebfab1ce3058dbe2b8dbb0ac
Manjot Singh <manjot.singh@tatadigital.com>
2021-11-25 08:26:21 +0530

    register & login api

--- dbconfig.js
+++ dbconfig.js
@@ -1,3 +1,5 @@
 module.exports = {
-  url: ""
+  url:
+    "mongodb+srv://hello:admin@cluster0.1wkyb.mongodb.net/demo?retryWrites=true&w=majority"
 };
+//mongodb+srv://<username>:<password>@cluster0.1wkyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
